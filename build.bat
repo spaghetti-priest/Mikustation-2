3f44862b0eaf2575c35fb8dcebd9f99143f5cecb
spaghetti-priest <xaviar.roach1@gmail.com>
2026-03-27 14:21:56 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 
 @echo off
-setlocal
+setlocal enabledelayedexpansion
+
 
 ::---------------------------------------------------------------------------
 :: Build notes
@@ -22,8 +23,8 @@ setlocal
 rem Ask if clang or msvc
 echo ------------------------------------------------------
 echo Building arguments:
-if "%1"=="msvc"  	   set msvc=1 		&& echo -MSVC
-if "%1"=="clang" 	   set clang=1 	&& echo -Clang
+if "%1"=="msvc"  	   set msvc=1 		&& echo [MSVC compiler selected]
+if "%1"=="clang" 	   set clang=1 	&& echo [Clang compiler selected]
 if "%2"=="debug" 	   set debug=1 	&& echo -Debug mode
 if "%2"=="release"   set release=1 	&& echo -Release mode
 if "%2"=="" 		   set release=1 	&& echo No build Configuration specified: Using debug mode
@@ -31,7 +32,7 @@ if "%2"=="" 		   set release=1 	&& echo No build Configuration specified: Using 
 if "%1"=="" 		   set msvc=1 		&& echo No compiler specified: Using MSVC
 if "%1"=="release" 	set msvc=1 		&& echo No compiler specified: Using MSVC with release mode
 
-if "%msvc%"=="1" 	   set clang=0
+if "%msvc%"=="1" 	   set clang=0 
 if "%debug%"=="1" 	set release=0
 
 echo ------------------------------------------------------
@@ -40,18 +41,19 @@ echo ------------------------------------------------------
 :: External Dependencies
 ::---------------------------------------------------------------------------
 set SDL2_DIR=extern\SDL2
-set EXTERN_DIR=extern
+set EXTERN_DIR=extern\
 set IMGUI_DIR=extern\\imgui
-set LIBS=/LIBPATH:"%SDL2_DIR%\\lib\\x64" SDL2.lib SDL2main.lib opengl32.lib shell32.lib
 
+set LIBS=/LIBPATH:"%SDL2_DIR%\\lib\\x64" SDL2.lib SDL2main.lib opengl32.lib shell32.lib
+set CLANG_LIBS=-L"%SDL2_DIR%\\lib\\x64" -lSDL2 -lSDL2main -lopengl32 -lshell32
 ::---------------------------------------------------------------------------
 :: Compiler and Linker Flags
 ::---------------------------------------------------------------------------
-set VC_COMPILER_FLAGS=  	/nologo /EHsc /Zi /MD /utf-8 /std:c++20
-set VC_LINKER_FLAGS=    	%LIBS% /incremental:no /opt:ref /subsystem:console
+set VC_COMPILER_FLAGS=/nologo /EHsc /Zi /MD /utf-8 /std:c++20
+set VC_LINKER_FLAGS=/link %LIBS% /incremental:no /opt:ref /subsystem:console
 
-set CLANG_COMPILER_FLAGS=  /nologo /EHsc /Zi /MD /utf-8 /std:c++20
-set CLANG_LINKER_FLAGS=    %LIBS% /incremental:no /opt:ref /subsystem:console
+set CLANG_COMPILER_FLAGS=-fexceptions -g --std=c++20 -finput-charset=UTF-8
+set CLANG_LINKER_FLAGS=%CLANG_LIBS% -Wl,/opt:ref -Wl,/subsystem:console
 
 ::---------------------------------------------------------------------------
 :: Sources and Includes
@@ -60,13 +62,30 @@ rem set INCLUDES= /I%EXTERN_DIR% /I%SDL2_DIR%\\include /I%FMT_DIR%\\include\\fmt
 set OUT_DIR=Debug
 set OUT_EXE=Mikustation-2
 
-set INCLUDES= /I%EXTERN_DIR% /I%IMGUI_DIR% /I%IMGUI_DIR%\\backends /I%SDL2_DIR%\\include
-set SOURCES=  %IMGUI_DIR%\\imgui.cpp src\\ps2.cpp %EXTERN_DIR%\\glad.c
+set VC_INCLUDES= /I%EXTERN_DIR% /I%IMGUI_DIR% /I%IMGUI_DIR%\\backends /I%SDL2_DIR%\\include
+set CLANG_INCLUDES=-I%EXTERN_DIR% -I%IMGUI_DIR% -I%IMGUI_DIR%\\backends -I%SDL2_DIR%\\include
+
+set SOURCES= %IMGUI_DIR%\\imgui.cpp src\\ps2.cpp %EXTERN_DIR%\\glad.c
 
 if not exist %OUT_DIR% mkdir %OUT_DIR%
 
 ::---------------------------------------------------------------------------
+:: Compiler selection
+::---------------------------------------------------------------------------
+set clang_debug=call clang++ -g -O0 %CLANG_COMPILER_FLAGS%
+set vc_debug=call cl /Od /Ob1 %VC_COMPILER_FLAGS%
+if "%msvc%" =="1"  set COMPILE=%vc_debug% 
+if "%msvc%" =="1"  set compile_link=%VC_LINKER_FLAGS%
+if "%msvc%" =="1"  set COMPILE_INCLUDES=%VC_INCLUDES%
+if "%msvc%" == "1" set out=/Fe%OUT_DIR%/%OUT_EXE%.exe /Fo%OUT_DIR%/
+
+if "%clang%" =="1" set COMPILE=%clang_debug%
+if "%clang%" =="1" set compile_link=%CLANG_LINKER_FLAGS%
+if "%clang%" =="1" set COMPILE_INCLUDES=%CLANG_INCLUDES%
+if "%clang%" == "1" set out=-o %OUT_DIR%/%OUT_EXE%.exe 
+::---------------------------------------------------------------------------
 :: Compile
 ::---------------------------------------------------------------------------
-call cl %VC_COMPILER_FLAGS% %INCLUDES% %SOURCES% /Fe%OUT_DIR%/%OUT_EXE%.exe /Fo%OUT_DIR%/ /link %VC_LINKER_FLAGS%
-rem call clang++ %SOURCES% -o %OUT_DIR%/%OUT_EXE%.exe
+:: FUck this if else statement man it just defaults to msvc for now
+rem cl %VC_COMPILER_FLAGS% %VC_INCLUDES% %SOURCES% /Fe%OUT_DIR%/%OUT_EXE%.exe /Fo%OUT_DIR%/ %VC_LINKER_FLAGS%
+clang++ %CLANG_COMPILER_FLAGS% %CLANG_INCLUDES% %SOURCES% -o %OUT_DIR%/%OUT_EXE%.exe %CLANG_LINKER_FLAGS%
